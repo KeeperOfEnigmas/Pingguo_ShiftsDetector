e3516f117d25cee6e5193e438eb446cbf95bd1c2
KeeperOfEnigmas <wilsonlin0725@gmail.com>
2024-11-13 18:40:45 +0100

    Update v1.2a - Code restructure.

--- pingguo/pingguo.py
+++ pingguo/pingguo.py
@@ -1,7 +1,5 @@
-# -*- coding: utf-8 -*-
 """
-Created on Sun Aug 25 12:21:57 2024
-
+Created on 2024.10.10 Thursday
 @author: 林威邑
 """
 
@@ -12,41 +10,59 @@ from selenium.webdriver.edge.options import Options
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.common.by import By
 import time
+import sys
 
 # constants
-subject = "苹果，你有新的工作，请及时处理！"
-body = "上班!!!"
-email = "kingarthur0323@gmail.com"
-email_password = "rbeyrnxthkkmmgxz"
+SUBJECT = "苹果，你有新的工作，请及时处理！"
+BODY = "上班!!!"
+EMAIL = "kingarthur0323@gmail.com"
+EMAIL_PASSWORD = "rbeyrnxthkkmmgxz"
 
-username = "hongpeng.li"
-password = "hgfdsa765432"
+USERNAME = "hongpeng.li"
+EMAIL_PASSWORD = "hgfdsa765432"
 
-url = "https://lothianeol.allocate-cloud.com/EmployeeOnlineMobile/LOTHIANLIVE/#/login"
-url_shifts = "https://lothianeol.allocate-cloud.com/EmployeeOnlineMobile/LOTHIANLIVE/#/bankshifts/shifts"
-path_to_driver_edge = "C:\\Users\\Arthur\\Desktop\\pingguo\\msedgedriver.exe"
+URL = "https://lothianeol.allocate-cloud.com/EmployeeOnlineMobile/LOTHIANLIVE/#/login"
+URL_SHIFTS = "https://lothianeol.allocate-cloud.com/EmployeeOnlineMobile/LOTHIANLIVE/#/bankshifts/shifts"
+PATH_TO_DRIVER_EDGE = "C:\\Users\\Arthur\\Desktop\\pingguo\\msedgedriver.exe"
 
-refresh_timer = 3 # in minutes
-shift_found = False
-    
-# send notification email
-def send_email(subject, body, email, password):
-    msg = MIMEText(body+" "+day+"!")
-    msg['Subject'] = subject
-    msg['From'] = email
-    msg['To'] = email
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+REFRESH_TIMER = 3  # in minutes
+
+
+def send_email(subject, body, day, email, password):
+    """
+    Send notification email.
+
+    `subject` -- title of the email \n
+    `body` -- content of the email \n
+    `day` -- the day of the shift \n
+    `email` -- emaili address \n
+    `password` -- passkey
+    """
+
+    msg = MIMEText(body + " " + day + "!")
+    msg["Subject"] = subject
+    msg["From"] = email
+    msg["To"] = email
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp_server:
         smtp_server.login(email, password)
         smtp_server.sendmail(email, email, msg.as_string())
     print("Email notification sent!")
 
-# detect if a shift is available
+
 def detect_shift(driver, refresh_timer, day):
+    """
+    Check if a shift exists on a given day.
+
+    `driver` -- driver for connecting to web browser \n
+    `refresh_timer` -- checking interval in minute \n
+    `day` -- the day of the shift
+    """
+
     curr_url = driver.current_url
-    if curr_url != url:
-        if curr_url != url_shifts:
-            driver.get(url)
-    
+    if curr_url != URL:
+        if curr_url != URL_SHIFTS:
+            driver.get(URL)
+
     while True:
         driver.refresh()
         time.sleep(10)
@@ -54,49 +70,66 @@ def detect_shift(driver, refresh_timer, day):
 
         elements_time = driver.find_elements(By.CLASS_NAME, "list")
 
-        for e_time in (elements_time):
+        for e_time in elements_time:
             try:
                 text = e_time.text
                 index = text.find(day)
                 subText = text[index:]
                 if "08:00" in subText:
                     print("Early shift found!")
-                    print("Shift starts from: " + text[index:index+len(day)])
+                    # print("Shift starts from: " + text[index:index+len(day)])
+                    print(f"Shift starts from: {text[index:index+len(day)]}")
 
                     shift_found = True
-                    send_email(subject, body, email, email_password)
+                    send_email(SUBJECT, BODY, day, EMAIL, EMAIL_PASSWORD)
                     terminate(driver)
-                    return        
-            except:
+            except Exception:
                 pass
 
-        if not(shift_found):
-            print("No shift found!")  
+        if not (shift_found):
+            print("No shift found!")
 
-        time.sleep(refresh_timer*60-10)
-        #time.sleep(5)
-    
-# terminate the program
+        time.sleep(refresh_timer * 60 - 10)
+        # time.sleep(5)
+
+
 def terminate(driver):
+    """
+    Stop the driver.
+
+    `driver` -- driver for connecting to web browser
+    """
+
     driver.quit()
-    print("Program terminate!")
-    
-# initialize edge driver 
+    print("Drivet terminated!")
+    sys.exit()
+
+
 def initialize_driver():
+    """
+    Initialize the driver.
+    """
+
     options = Options()
     options.add_experimental_option("debuggerAddress", "localhost:9222")
-    driver_path = path_to_driver_edge
-    driver = webdriver.Edge(options=options, service=Service(driver_path))    
+    driver = webdriver.Edge(options=options, service=Service(PATH_TO_DRIVER_EDGE))
     return driver
 
 
+def main():
+    """
+    Main function.
+    """
 
-try:
-    day = input("Enter which day you want(capitalized): ")
-    print("Program running!")
-    driver = initialize_driver()
-    detect_shift(driver, refresh_timer, day)
-except KeyboardInterrupt:
-    terminate(driver)
-    
-    
+    try:
+        day = input("Enter which day you want(capitalized): ")
+        print("Program running!")
+        driver = initialize_driver()
+        detect_shift(driver, REFRESH_TIMER, day)
+        return 0
+    except KeyboardInterrupt:
+        terminate(driver)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
